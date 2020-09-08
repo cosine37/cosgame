674703fa54f8f8225cae5588e74ad48f1c42029f
cosine37 <konglinghao111222@gmail.com>
2020-09-08 16:29:06 -0400

    #0208, #0219 add Splendor & Nothanks page

---
+++ src/main/webapp/js/app/splendorMainController.js
@@ -0,0 +1,26 @@
+var setUrl = function(d){
+	hearder = "http://";
+	server = "localhost:8080";
+	return header + server + d;
+}
+
+var app = angular.module("splendorMainApp", []);
+app.controller("splendorMainCtrl", ['$scope', '$window', '$http', '$document',
+	function($scope, $window, $http, $document){
+	
+		$scope.goto = function(d){
+			var x = "http://" + $window.location.host;
+			$window.location.href = x + "/" + d;
+		}
+		
+		$http.post('/username').then(function(response){
+			$scope.username = response.data.value[0];
+		});
+		
+		$scope.logout = function(){
+			$http({url: "/logout", method: "POST"}).then(function(response){
+				$scope.goto('login');
+			});
+		}
+		
+}]);
