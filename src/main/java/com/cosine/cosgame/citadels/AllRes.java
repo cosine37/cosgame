c77aee3cd5600edcc91764f73cf08f0845d8643f
cosine37 <konglinghao111222@gmail.com>
2020-09-16 22:20:52 -0400

    #0205 update card info & news

--- src/main/java/com/cosine/cosgame/citadels/AllRes.java
+++ src/main/java/com/cosine/cosgame/citadels/AllRes.java
@@ -137,7 +137,7 @@ public class AllRes {
 		specialCards.add(c);
 		c = new Insurance();
 		specialCards.add(c);
-		/*
+		
 		c = new Museum();
 		specialCards.add(c);
 		c = new Village();
@@ -157,7 +157,7 @@ public class AllRes {
 		specialCards.add(c);
 		c = new StupidSun();
 		specialCards.add(c);
-		*/
+		
 	}
 	
 	public void setBaseCards() {
